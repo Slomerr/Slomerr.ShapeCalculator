523fcc364967e0f9ce620224ad4bac792931c71f
Vladislav Fisher <vladislav.fisher@testdlab.com>
2023-11-21 11:49:08 +0700

    optimization get pairs

--- SqlRequests/GetPairs.sql
+++ SqlRequests/GetPairs.sql
@@ -1,10 +1,3 @@
-WITH all_pairs AS (
-    SELECT product_id, 0 AS category_id
-    FROM product
-    WHERE product_id NOT IN (SELECT product_id FROM product_category)
-    UNION
-    SELECT * FROM product_category
-)
-SELECT name_product, name_category FROM all_pairs
-LEFT JOIN product ON all_pairs.product_id = product.product_id
-LEFT JOIN category ON all_pairs.category_id = category.category_id
+SELECT name_product, name_category FROM product
+LEFT JOIN product_category ON product.product_id = product_category.product_id
+LEFT JOIN category ON product_category.category_id = category.category_id
