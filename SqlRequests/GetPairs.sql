91549efa088829314619f993c1238c3cea9c0bc6
Vladislav Fisher <vladislav.fisher@testdlab.com>
2023-11-21 11:32:01 +0700

    fix sql

--- SqlRequests/GetPairs.sql
+++ SqlRequests/GetPairs.sql
@@ -1,4 +1,4 @@
-WITH all_pairs AS(
+WITH all_pairs AS (
     SELECT product_id, 0 AS category_id
     FROM product
     WHERE product_id NOT IN (SELECT product_id FROM product_category)
@@ -6,5 +6,5 @@ WITH all_pairs AS(
     SELECT * FROM product_category
 )
 SELECT name_product, name_category FROM all_pairs
-LEFT JOIN product USING (product_id)
-LEFT JOIN category USING (category_id)
+LEFT JOIN product ON all_pairs.product_id = product.product_id
+LEFT JOIN category ON all_pairs.category_id = category.category_id
